0bf23315d4816193bcd580c94cc25e70ff5518ec
leixu <leix8@uci.edu>
2025-08-12 17:55:03 +0000

    add dlc_linux to dlc_android compare

--- validate_embeddings.py
+++ validate_embeddings.py
@@ -35,10 +35,33 @@ if __name__ == "__main__":
                 tensor2 = torch.tensor(onnx_linux_json[image_path])
                 cos_sim, l2_dist, scale_diff = compute_similarity(tensor1, tensor2)
                 visualize_and_save(image_path = image_path, cos_sim = cos_sim, scale_diff = scale_diff, output_path = os.path.join(args.output_dir, base_name), mode = mode)
-                
+    
+    if args.onnx_linux and args.dlc_linux:
+        with open(args.onnx_linux, "r") as f:
+            onnx_linux_json = json.load(f)
+        for image_path, onnx_linux_array in onnx_linux_json.items():
+            mode = "onnx_linux_vs_dlc_onnx"
+            base_name = os.path.splitext(os.path.basename(image_path))[0] + f"_{mode}.png"
+            onnx_linux_tensor = torch.tensor(onnx_linux_array)
+
+            dlc_linux_array = np.fromfile(args.dlc_linux, dtype=np.float32)
+            dlc_linux_tensor = torch.tensor(dlc_linux_array).reshape(onnx_linux_tensor.shape)
+
+            cos_sim, l2_dist, scale_diff = compute_similarity(onnx_linux_tensor, dlc_linux_tensor)
+            visualize_and_save(image_path = image_path, cos_sim = cos_sim, scale_diff = scale_diff, output_path = os.path.join(args.output_dir, base_name), mode = mode)
+            
     if args.dlc_linux and args.dlc_android:
+        mode = "dlc_linux_vs_dlc_android"
+        base_name = f"emulation_{mode}.png"
         dlc_linux_array = np.fromfile(args.dlc_linux, dtype=np.float32)
         dlc_android_array = np.fromfile(args.dlc_android, dtype=np.float32)
         print(f"check loaded dlc_linux_array: shape = {dlc_linux_array.shape}, type = {dlc_linux_array.dtype}")
         print(f"check loaded dlc_android_array: shape = {dlc_android_array.shape}, type = {dlc_android_array.dtype}")
+        shape = (256, 3072)
+        dlc_linux_tensor = torch.tensor(dlc_linux_array).reshape(shape)
+        dlc_android_tensor = torch.tensor(dlc_android_array).reshape(shape)
+        cos_sim, l2_dist, scale_diff = compute_similarity(dlc_linux_tensor, dlc_android_tensor)
+        visualize_and_save(cos_sim = cos_sim, scale_diff = scale_diff, output_path = os.path.join(args.output_dir, base_name), mode = mode)
+
+
         
